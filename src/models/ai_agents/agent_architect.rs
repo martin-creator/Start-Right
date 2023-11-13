152efa6dc2255deea6a8fd9d6187fda932e10546
martin-creator <martinlubowa@outlook.com>
2023-11-13 23:53:00 +0300

    Implemented AgentSolutionArchitect

--- src/models/ai_agents/agent_architect.rs
+++ src/models/ai_agents/agent_architect.rs
@@ -4,3 +4,62 @@ use crate::helper_funcs::general_funcs::{ai_task_request_decoded, check_status_c
 use crate::models::ai_agent_skeleton::basic_agent::{AgentState, BasicAgent};
 use crate::models::ai_agent_skeleton::basic_traits::BasicTraits;
 use crate::models::ai_agents::agent_content_traits::{FactSheet, ProjectScope, SpecialFunctions};
+
+use async_trait::async_trait;
+use reqwest::Client;
+use std::time::Duration;
+
+// Solutions Architect
+#[derive(Debug)]
+pub struct AgentSolutionArchitect {
+    attributes: BasicAgent,
+}
+
+impl AgentSolutionArchitect {
+    pub fn new() -> Self {
+        let attributes: BasicAgent = BasicAgent {
+            objective: "Gathers information and design solutions for website development"
+                .to_string(),
+            position: "Solutions Architect".to_string(),
+            state: AgentState::Discovery,
+            memory: vec![],
+        };
+
+        Self { attributes }
+    }
+
+    // Retrieve Project Scope
+    async fn call_project_scope(&mut self, factsheet: &mut FactSheet) -> ProjectScope {
+        let msg_context: String = format!("{}", factsheet.project_description);
+
+        let ai_response: ProjectScope = ai_task_request_decoded::<ProjectScope>(
+            msg_context,
+            &self.attributes.position,
+            get_function_string!(print_project_scope),
+            print_project_scope,
+        )
+        .await;
+
+        factsheet.project_scope = Some(ai_response.clone());
+        self.attributes.update_state(AgentState::Finished);
+        return ai_response;
+    }
+
+    // Retrieve Project Scope
+    async fn call_determine_external_urls(
+        &mut self,
+        factsheet: &mut FactSheet,
+        msg_context: String,
+    ) {
+        let ai_response: Vec<String> = ai_task_request_decoded::<Vec<String>>(
+            msg_context,
+            &self.attributes.position,
+            get_function_string!(print_site_urls),
+            print_site_urls,
+        )
+        .await;
+
+        factsheet.external_urls = Some(ai_response);
+        self.attributes.state = AgentState::UnitTesting;
+    }
+}
