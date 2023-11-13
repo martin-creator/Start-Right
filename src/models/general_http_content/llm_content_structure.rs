3a19771abe1a2609518079a980a0ccf9c3b55ba7
martin-creator <martinlubowa@outlook.com>
2023-11-13 16:11:52 +0300

    Created http content structure to send to llm

---
+++ src/models/general_http_content/llm_content_structure.rs
@@ -0,0 +1,34 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Clone)]
+/// Message struct sending messages openai
+pub struct Message {
+    pub role: String,
+    pub content: String,
+}
+
+#[derive(Debug, Serialize, Clone)]
+/// ChatCompletion struct sending messages openai
+pub struct ChatCompletion {
+    pub model: String,
+    pub messages: Vec<Message>,
+    pub temperature: f32,
+}
+
+#[derive(Debug, Deserialize)]
+/// ChatResponse struct receiving messages from openai
+pub struct APIMessage {
+    pub content: String,
+}
+
+#[derive(Debug, Deserialize)]
+/// ChatResponse struct receiving messages from openai
+pub struct APIChoice {
+    pub message: APIMessage,
+}
+
+#[derive(Debug, Deserialize)]
+/// ChatResponse struct receiving messages from openai
+pub struct APIResponse {
+    pub choices: Option<Vec<APIChoice>>,
+}
