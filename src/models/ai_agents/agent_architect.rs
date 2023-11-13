fa1786474c835adc694801bdbd946989c5728ec4
martin-creator <martinlubowa@outlook.com>
2023-11-14 00:12:07 +0300

    Added tests for solution architect

--- src/models/ai_agents/agent_architect.rs
+++ src/models/ai_agents/agent_architect.rs
@@ -154,3 +154,31 @@ impl SpecialFunctions for AgentSolutionArchitect {
         Ok(())
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn tests_solution_architect() {
+        let mut agent: AgentSolutionArchitect = AgentSolutionArchitect::new();
+
+        let mut factsheet: FactSheet = FactSheet {
+      project_description: "Build a full stack website with user login and logout that shows latest Forex prices".to_string(),
+      project_scope: None,
+      external_urls: None,
+      backend_code: None,
+      api_endpoint_schema: None,
+    };
+
+        agent
+            .execute(&mut factsheet)
+            .await
+            .expect("Unable to execute Solutions Architect Agent");
+        assert!(factsheet.project_scope != None);
+        assert!(factsheet.external_urls.is_some());
+
+        dbg!(factsheet);
+    }
+}
