ad348bbd93999d0e96646f70a797ce9d8a01be8e
martin-creator <martinlubowa@outlook.com>
2023-11-13 18:47:25 +0300

    Fixed file module importation bugs

--- src/helper_funcs/general_funcs.rs
+++ src/helper_funcs/general_funcs.rs
@@ -1,6 +1,5 @@
-use crate::apis::call_request::call_gpt;
-use crate::helpers::command_line::PrintCommand;
-use crate::models::general::llm::Message;
+use crate::helper_funcs::cli_funcs::PrintCommand;
+use crate::models::general_http_content::llm_content_structure::Message;
 use reqwest::Client;
 use serde::de::DeserializeOwned;
 use std::fs;
@@ -17,3 +16,25 @@ pub const EXEC_MAIN_PATH: &str = "src/main_web_template.rs";
 pub const MAIN_INDEX_PATH: &str = "frontend/index.html";
 
 pub const API_SCHEMA_PATH: &str = "schemas/api_schema.json";
+
+
+/// Extended AI function to encourage specific output from LLMS
+pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) -> Message {
+    let ai_function_str: &str = ai_func(func_input);
+
+    // Extend the string to encourage only printing the output
+    let msg: String = format!(
+        "FUNCTION: {}
+  INSTRUCTION: You are a function printer. You ONLY print the results of functions.
+  Nothing else. No commentary. Here is the input to the function: {}.
+  Print out what the function will return.",
+        ai_function_str, func_input
+    );
+
+    // Return message
+    Message {
+        role: "system".to_string(),
+        content: msg,
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#[macro_export]
+macro_rules! get_function_string {
+    ($func: ident) => {{
+        stringify!($func)
+    }};
+}
+
+#[macro_use]
+mod ai_macros;
+// mod apis;
+mod helper_funcs;
+mod models;
+
+
 fn main(){
     println!("Hello, world!");
 }
