bd770e6bd8305a0b6930f27b9288ec85dd2e3504
martin-creator <martinlubowa@outlook.com>
2023-11-14 14:37:30 +0300

    Imported backend agent modules

---
+++ src/models/ai_agents/agent_backend.rs
@@ -0,0 +1,19 @@
+use crate::ai_macro::ai_backend::{
+    print_backend_webserver_code, print_fixed_code, print_improved_webserver_code,
+    print_rest_api_endpoints,
+};
+use crate::helper_funcs::general_funcs::{
+    check_status_code, read_backend_code_template_contents, read_exec_main_contents, save_api_endpoints,
+    save_backend_code, WEB_SERVER_PROJECT_PATH,
+};
+
+use crate::helper_funcs::cli_funcs::{confirm_safe_code, PrintCommand};
+use crate::helper_funcs::general_funcs::ai_task_request;
+use crate::models::ai_agent_skeleton::basic_agent::{AgentState, BasicAgent};
+use crate::models::ai_agents::agent_content_traits::{FactSheet, RouteObject, SpecialFunctions};
+
+use async_trait::async_trait;
+use reqwest::Client;
+use std::process::{Command, Stdio};
+use std::time::Duration;
+use tokio::time;
--- src/models/ai_agents/mod.rs
+++ src/models/ai_agents/mod.rs
@@ -1,2 +1,3 @@
 pub mod agent_content_traits;
 pub mod agent_architect;
+pub mod agent_backend;
