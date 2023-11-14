ad014ed05d9a1dbe620926f429040175be88e46f
martin-creator <martinlubowa@outlook.com>
2023-11-14 20:50:52 +0300

    completed the backend developer agent

--- src/models/agents_manager/agent_manager.rs
+++ src/models/agents_manager/agent_manager.rs
@@ -4,7 +4,7 @@ use crate::models::ai_agents::agent_content_traits::{FactSheet, SpecialFunctions
 use crate::ai_macros::ai_managing::convert_user_input_to_goal;
 use crate::helper_funcs::general_funcs::ai_task_request;
 use crate::models::ai_agents::agent_architect::AgentSolutionArchitect;
-// use crate::models::ai_agents::agent_backend::AgentBackendDeveloper;
+use crate::models::ai_agents::agent_backend::AgentBackendDeveloper;
 
 #[derive(Debug)]
 pub struct ManagingAgent {
@@ -56,7 +56,7 @@ impl ManagingAgent {
 
     fn create_agents(&mut self) {
         self.add_agent(Box::new(AgentSolutionArchitect::new()));
-        //self.add_agent(Box::new(AgentBackendDeveloper::new()));
+        self.add_agent(Box::new(AgentBackendDeveloper::new()));
     }
 
     pub async fn execute_project(&mut self) {
