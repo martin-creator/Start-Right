9158cc91f0afedb96bf9fe2559fc0aa7e3553a53
martin-creator <martinlubowa@outlook.com>
2023-11-14 20:14:05 +0300

    Implemented core funcs fro backend developer

--- src/models/ai_agents/agent_backend.rs
+++ src/models/ai_agents/agent_backend.rs
@@ -25,3 +25,95 @@ pub struct AgentBackendDeveloper {
     bug_errors: Option<String>,
     bug_count: u8,
 }
+
+impl AgentBackendDeveloper {
+    pub fn new() -> Self {
+        let attributes: BasicAgent = BasicAgent {
+            objective: "Develops backend code for webserver and json database".to_string(),
+            position: "Backend Developer".to_string(),
+            state: AgentState::Discovery,
+            memory: vec![],
+        };
+
+        Self {
+            attributes,
+            bug_errors: None,
+            bug_count: 0,
+        }
+    }
+
+    async fn call_initial_backend_code(&mut self, factsheet: &mut FactSheet) {
+        let code_template_str: String = read_code_template_contents();
+
+        // Concatenate Instruction
+        let msg_context: String = format!(
+            "CODE TEMPLATE: {} \n PROJECT_DESCRIPTION: {} \n",
+            code_template_str, factsheet.project_description
+        );
+
+        let ai_response: String = ai_task_request(
+            msg_context,
+            &self.attributes.position,
+            get_function_string!(print_backend_webserver_code),
+            print_backend_webserver_code,
+        )
+        .await;
+
+        save_backend_code(&ai_response);
+        factsheet.backend_code = Some(ai_response);
+    }
+
+    async fn call_improved_backend_code(&mut self, factsheet: &mut FactSheet) {
+        let msg_context: String = format!(
+            "CODE TEMPLATE: {:?} \n PROJECT_DESCRIPTION: {:?} \n",
+            factsheet.backend_code, factsheet
+        );
+
+        let ai_response: String = ai_task_request(
+            msg_context,
+            &self.attributes.position,
+            get_function_string!(print_improved_webserver_code),
+            print_improved_webserver_code,
+        )
+        .await;
+
+        save_backend_code(&ai_response);
+        factsheet.backend_code = Some(ai_response);
+    }
+
+    async fn call_fix_code_bugs(&mut self, factsheet: &mut FactSheet) {
+        let msg_context: String = format!(
+            "BROKEN_CODE: {:?} \n ERROR_BUGS: {:?} \n
+      THIS FUNCTION ONLY OUTPUTS CODE. JUST OUTPUT THE CODE.",
+            factsheet.backend_code, self.bug_errors
+        );
+
+        let ai_response: String = ai_task_request(
+            msg_context,
+            &self.attributes.position,
+            get_function_string!(print_fixed_code),
+            print_fixed_code,
+        )
+        .await;
+
+        save_backend_code(&ai_response);
+        factsheet.backend_code = Some(ai_response);
+    }
+
+    async fn call_extract_rest_api_endpoints(&self) -> String {
+        let backend_code: String = read_exec_main_contents();
+
+        // Structure message context
+        let msg_context: String = format!("CODE_INPUT: {}", backend_code);
+
+        let ai_response: String = ai_task_request(
+            msg_context,
+            &self.attributes.position,
+            get_function_string!(print_rest_api_endpoints),
+            print_rest_api_endpoints,
+        )
+        .await;
+
+        ai_response
+    }
+}
