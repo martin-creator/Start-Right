e7c927169e8824cb8c0b09bedd9031be000b5512
martin-creator <martinlubowa@outlook.com>
2023-11-15 13:45:02 +0300

    created function to read api json

--- src/models/ai_agents/agent_frontend.rs
+++ src/models/ai_agents/agent_frontend.rs
@@ -1,7 +1,6 @@
 use crate::ai_macros::ai_frontend::{print_fixed_frontend_code, print_frontend_code, print_index_html_file, print_improved_frontend_code};
 use crate::helper_funcs::general_funcs::{
-    check_status_code, read_backend_code_template_contents, read_exec_main_contents, save_api_endpoints,
-    save_backend_code, MAIN_INDEX_PATH
+    check_status_code, read_frontend_code_template_contents,   read_main_index_contents,  save_frontend_code, MAIN_INDEX_PATH
 };
 
 use crate::helper_funcs::cli_funcs::{confirm_safe_code, PrintCommand};
@@ -36,5 +35,27 @@ impl AgentFrontendDeveloper {
             bug_errors: None,
             bug_count: 0,
         }
+    }
+
+    async fn call_initial_frontend_code(&mut self, factsheet: &mut FactSheet) {
+        let index_template_str: String = read_main_index_contents();
+
+        //PROJECT_DESCRIPTION, INDEX_TEMPLATE and API_JSON Schema
+        // Concatenate Instruction
+        let msg_context: String = format!(
+            "INDEX_TEMPLATE: {} \n  API_JSON Schema {} \n PROJECT_DESCRIPTION: {} \n",
+            index_template_str, factsheet.project_description
+        );
+
+        let ai_response: String = ai_task_request(
+            msg_context,
+            &self.attributes.position,
+            get_function_string!(print_backend_webserver_code),
+            print_backend_webserver_code,
+        )
+        .await;
+
+        save_backend_code(&ai_response);
+        factsheet.backend_code = Some(ai_response);
     }
 }
