8ad5abf28c6c8e139ed4c164463693c51f5fb284
martin-creator <martinlubowa@outlook.com>
2023-11-16 20:57:17 +0300

    react_macro to module

---
+++ src/models/ai_agents/agent_react_frontend.rs
@@ -0,0 +1,15 @@
+use crate::ai_macros::ai_react_frontend::{
+    print_react_code_bugs_resolution,
+    print_react_recommended_site_pages,
+    print_react_recommended_site_pages_with_apis,
+    print_react_recommended_site_main_colours,
+};
+
+use crate::helper_funcs::general_funcs::{
+    save_react_frontend_code,
+    ai_task_request_decoded,
+    ai_task_request,
+    read_react_frontend_code_contents,
+    WEB_SERVER_PROJECT_PATH,
+    REACT_FRONTEND_PATH,
+};
