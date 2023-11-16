22eb6f5cb8c675778b53dc90739e59eeb337520c
martin-creator <martinlubowa@outlook.com>
2023-11-16 21:13:35 +0300

    Updated the added functions for react frontend developer

--- src/models/ai_agents/agent_react_frontend.rs
+++ src/models/ai_agents/agent_react_frontend.rs
@@ -24,3 +24,50 @@ use std::fs;
 use std::process::{Command, Stdio};
 use std::collections::HashMap;
 use strum::IntoEnumIterator;
+
+/// To define what stage the frontend developer is at
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum FrontendBuildMode {
+  Infrastructure,
+  PageComponents,
+  Completion
+}
+
+
+/// For decoding the serde_json api routes for a given page
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct APIAssignment {
+  pub api_route: String,
+  pub method: String,
+  pub route_type: String,
+}
+
+
+/// Used for creating a type to be used for decoding shorthand
+type PageRoutes = HashMap<String, Vec<APIAssignment>>;
+
+
+/// Used for decoding page names and suggested content
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct PageAPIAssign {
+  pub page: Vec<APIAssignment>
+}
+
+
+/// Used for decoding page names and suggested content
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct SitePages {
+  pub page_name: String,
+  pub suggested_content_sections: serde_json::Value
+}
+
+
+/// Used for DesignBuildSheet
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct DesignBuildSheet {
+  pub pages: Option<Vec<String>>,
+  pub pages_descriptons: Option<Vec<SitePages>>,
+  pub api_assignments: Option<PageRoutes>,
+  pub brand_colours: Option<Vec<String>>,
+  pub build_mode: FrontendBuildMode
+}
