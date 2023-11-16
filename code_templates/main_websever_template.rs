865c1a97f13ee5997be3c4ce6003b08f4d5179d7
martin-creator <martinlubowa@outlook.com>
2023-11-16 13:23:33 +0300

    Updated main web server template with sleep after 60 seconds

--- code_templates/main_websever_template.rs
+++ code_templates/main_websever_template.rs
@@ -1,80 +1,81 @@
 use actix_cors::Cors;
-use actix_web::{ http::header, web, App, HttpResponse, HttpServer, Responder };
+
+use actix_web::{ http::header, web, App, HttpServer, Responder, HttpResponse };
+
 use serde::{ Deserialize, Serialize };
-use std::sync::Mutex;
-use std::{ collections::HashMap, fs, io::Write };
+
+use reqwest::Client as HttpClient;
+
+use async_trait::async_trait;
+
 use actix_files::Files;
 
-// Define the Task structure for representing tasks in the system.
+use tokio::time::Duration;
+
+use std::sync::Mutex;
+use std::collections::HashMap;
+use std::fs;
+use std::io::Write;
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct Task {
     id: u64,
     name: String,
-    completed: bool,
+    completed: bool
 }
 
-// Define the User structure for representing users in the system.
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct User {
     id: u64,
     username: String,
-    // In a production environment, store hashed passwords, not plaintext.
-    password: String,
+    password: String
 }
 
-// Define the Database structure to hold tasks and users.
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct Database {
     tasks: HashMap<u64, Task>,
-    users: HashMap<u64, User>,
+    users: HashMap<u64, User>
 }
 
-// Implement methods for the Database structure to perform CRUD operations.
 impl Database {
-    // Create a new instance of the database.
     fn new() -> Self {
         Self {
             tasks: HashMap::new(),
-            users: HashMap::new(),
+            users: HashMap::new()
         }
     }
 
-    // Insert a new task into the database.
+    // CRUD DATA
     fn insert(&mut self, task: Task) {
         self.tasks.insert(task.id, task);
     }
 
-    // Retrieve a task by its ID.
     fn get(&self, id: &u64) -> Option<&Task> {
         self.tasks.get(id)
     }
 
-    // Retrieve all tasks in the database.
     fn get_all(&self) -> Vec<&Task> {
         self.tasks.values().collect()
     }
 
-    // Delete a task by its ID.
     fn delete(&mut self, id: &u64) {
         self.tasks.remove(id);
     }
 
-    // Update an existing task in the database.
     fn update(&mut self, task: Task) {
         self.tasks.insert(task.id, task);
     }
 
-    // Insert a new user into the database.
+    // USER DATA RELATED FUNCTIONS
     fn insert_user(&mut self, user: User) {
         self.users.insert(user.id, user);
     }
 
-    // Retrieve a user by their username.
     fn get_user_by_name(&self, username: &str) -> Option<&User> {
         self.users.values().find(|u| u.username == username)
     }
 
-    // Save the database content to a file.
+    // DATABASE SAVING
     fn save_to_file(&self) -> std::io::Result<()> {
         let data: String = serde_json::to_string(&self)?;
         let mut file: fs::File = fs::File::create("database.json")?;
@@ -82,7 +83,6 @@ impl Database {
         Ok(())
     }
 
-    // Load the database content from a file.
     fn load_from_file() -> std::io::Result<Self> {
         let file_content: String = fs::read_to_string("database.json")?;
         let db: Database = serde_json::from_str(&file_content)?;
@@ -90,121 +90,77 @@ impl Database {
     }
 }
 
-// Define the application state structure to hold the database.
 struct AppState {
-    db: Mutex<Database>,
+    db: Mutex<Database>
 }
 
-// Helper function to construct a JSON response.
-fn json_response<T: Serialize>(data: T) -> HttpResponse {
-    HttpResponse::Ok().json(data)
-}
-
-// Endpoint to create a new task.
-// @desc    Create a new task
-// @route   POST /api/v1/tasks
-// @access  Public
 async fn create_task(app_state: web::Data<AppState>, task: web::Json<Task>) -> impl Responder {
     let mut db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
     db.insert(task.into_inner());
     let _ = db.save_to_file();
-    json_response("Task created successfully")
+    HttpResponse::Ok().finish()
 }
 
-// Endpoint to get a task by ID.
-// @desc    Get a task by ID
-// @route   GET /api/v1/tasks/{id}
-// @access  Public
 async fn read_task(app_state: web::Data<AppState>, id: web::Path<u64>) -> impl Responder {
     let db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
     match db.get(&id.into_inner()) {
-        Some(task) => json_response(task),
-        None => HttpResponse::NotFound().json("Task not found"),
+        Some(task) => HttpResponse::Ok().json(task),
+        None => HttpResponse::NotFound().finish()
     }
 }
 
-// Endpoint to get all tasks.
-// @desc    Get all tasks
-// @route   GET /api/v1/tasks
-// @access  Public
 async fn read_all_tasks(app_state: web::Data<AppState>) -> impl Responder {
     let db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
     let tasks = db.get_all();
-    json_response(tasks)
+    HttpResponse::Ok().json(tasks)
 }
 
-// Endpoint to update a task.
-// @desc    Update a task
-// @route   PUT /api/v1/tasks
-// @access  Public
 async fn update_task(app_state: web::Data<AppState>, task: web::Json<Task>) -> impl Responder {
     let mut db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
     db.update(task.into_inner());
     let _ = db.save_to_file();
-    json_response("Task updated successfully")
+    HttpResponse::Ok().finish()
 }
 
-// Endpoint to delete a task by ID.
-// @desc    Delete a task by ID
-// @route   DELETE /api/v1/tasks/{id}
-// @access  Public
 async fn delete_task(app_state: web::Data<AppState>, id: web::Path<u64>) -> impl Responder {
     let mut db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
     db.delete(&id.into_inner());
     let _ = db.save_to_file();
-    json_response("Task deleted successfully")
+    HttpResponse::Ok().finish()
 }
 
-// Endpoint to register a new user.
-// @desc    Register user
-// @route   POST /api/v1/auth/register
-// @access  Public
 async fn register(app_state: web::Data<AppState>, user: web::Json<User>) -> impl Responder {
     let mut db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
-
-    // Check if the username is already taken
-    if db.get_user_by_name(&user.username).is_some() {
-        return HttpResponse::Conflict().json("Username already exists");
-    }
-
     db.insert_user(user.into_inner());
     let _ = db.save_to_file();
-    json_response("User registered successfully")
+    HttpResponse::Ok().finish()
 }
 
-// Endpoint for user login.
-// @desc    User login
-// @route   POST /api/v1/auth/login
-// @access  Public
 async fn login(app_state: web::Data<AppState>, user: web::Json<User>) -> impl Responder {
     let db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
     match db.get_user_by_name(&user.username) {
         Some(stored_user) if stored_user.password == user.password => {
-            json_response("Logged in successfully")
-        }
-        _ => HttpResponse::Unauthorized().json("Invalid username or password"),
+            HttpResponse::Ok().body("Logged in!")
+        },
+        _ => HttpResponse::BadRequest().body("Invalid username or password")
     }
 }
 
-// Entry point for the Actix web server.
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Load the database from a file or create a new one if not found.
+
     let db: Database = match Database::load_from_file() {
         Ok(db) => db,
-        Err(_) => Database::new(),
+        Err(_) => Database::new()
     };
 
-    // Create the Actix web application state with a mutex-protected database.
     let data: web::Data<AppState> = web::Data::new(AppState {
-        db: Mutex::new(db),
+        db: Mutex::new(db)
     });
 
-    // Start the Actix web server.
-    HttpServer::new(move || {
+    let server = HttpServer::new(move || {
         App::new()
             .wrap(
-                // Configure CORS settings.
                 Cors::permissive()
                     .allowed_origin_fn(|origin, _req_head| {
                         origin.as_bytes().starts_with(b"http://localhost") || origin == "null"
@@ -217,11 +173,10 @@ async fn main() -> std::io::Result<()> {
             )
             .app_data(data.clone())
             .service(
-                // Serve static files from the "static" directory.
                 Files::new("/", "frontend/").index_file("index.html")
             )
             .service(
-                // Define the API routes and corresponding handlers.
+                
                 web
                     ::scope("/api/v1")
                     .service(
@@ -241,6 +196,12 @@ async fn main() -> std::io::Result<()> {
                     .service(web::resource("/auth/login").route(web::post().to(login)))
             )
     })
-        .bind("localhost:8080")?
-        .run().await
+    .bind("localhost:8080")?
+    .run();
+    // Spawn a background task to sleep after the server has run for 60 seconds.
+    tokio::spawn(async move {
+        tokio::time::sleep(Duration::from_secs(60)).await;
+        println!("Server has run for 60 seconds. Now sleeping.");
+    });
+    server.await
 }
