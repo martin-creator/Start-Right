6e23402eabae2f2e6e2d06b01642d4291fa38329
martin-creator <martinlubowa@outlook.com>
2023-11-16 21:37:00 +0300

    Created react frontend matching

--- src/models/ai_agents/agent_react_frontend.rs
+++ src/models/ai_agents/agent_react_frontend.rs
@@ -15,63 +15,70 @@ use crate::helper_funcs::general_funcs::{
 };
 
 use crate::models::ai_agents::agent_react_structs::{
-    FrontendBuildMode, APIAssignment, PageAPIAssign, SitePages, PageRoutes, DesignBuildSheet 
+    FrontendBuildMode,
+    APIAssignment,
+    PageAPIAssign,
+    SitePages,
+    PageRoutes,
+    DesignBuildSheet,
 };
 
 //use crate::models::agents::agent_frontend_comp::BuildComponent;
-use crate::models::ai_agents::agent_content_traits::{SpecialFunctions, FactSheet};
-use crate::models::ai_agent_skeleton::basic_agent::{BasicAgent, AgentState};
+use crate::models::ai_agents::agent_content_traits::{ SpecialFunctions, FactSheet };
+use crate::models::ai_agent_skeleton::basic_agent::{ BasicAgent, AgentState };
 use crate::helper_funcs::cli_funcs::PrintCommand;
 use async_trait::async_trait;
-use serde::{Serialize, Deserialize};
+use serde::{ Serialize, Deserialize };
 use std::fs;
-use std::process::{Command, Stdio};
+use std::process::{ Command, Stdio };
 use std::collections::HashMap;
 use strum::IntoEnumIterator;
 
 // use super::agent_react_structs::FrontendBuildMode;
 
-
-
-
 // React Frontend Developer
 #[derive(Debug)]
 pub struct AgentReactFrontendDeveloper {
-  pub attributes: BasicAgent,
-  pub buildsheet: DesignBuildSheet,
-  pub bug_count: u8,
-  //pub operation_focus: BuildComponent
+    pub attributes: BasicAgent,
+    pub buildsheet: DesignBuildSheet,
+    pub bug_count: u8,
+    //pub operation_focus: BuildComponent
 }
 
-
-impl  AgentReactFrontendDeveloper {
+impl AgentReactFrontendDeveloper {
     pub fn new() -> Self {
-
         // Define attributes
         let attributes: BasicAgent = BasicAgent {
-          objective: "Develops raect frontned code for website".to_string(),
-          position: "React Frontend Developer".to_string(),
-          state: AgentState::Discovery,
-          memory: vec![]
+            objective: "Develops react frontned code for website".to_string(),
+            position: "React Frontend Developer".to_string(),
+            state: AgentState::Discovery,
+            memory: vec![],
         };
-    
+
         // Define Buildsheet
         let buildsheet: DesignBuildSheet = DesignBuildSheet {
-          pages: None,
-          pages_descriptons: None,
-          api_assignments: None,
-          brand_colours: None,
-          build_mode: FrontendBuildMode::Infrastructure
+            pages: None,
+            pages_descriptons: None,
+            api_assignments: None,
+            brand_colours: None,
+            build_mode: FrontendBuildMode::Infrastructure,
         };
-    
+
         // Return Self
         Self {
-          attributes,
-          buildsheet,
-          bug_count: 0,
-          //operation_focus: BuildComponent::Logo
+            attributes,
+            buildsheet,
+            bug_count: 0,
+            //operation_focus: BuildComponent::Logo
         }
-      }
-    
-}
+    }
 
+    /// Confirms what stage the Frontend Agent is in
+    fn confirm_stage(&self) {
+        match self.buildsheet.build_mode {
+            FrontendBuildMode::Infrastructure => println!("[Working on Frontend Infrastructure]"),
+            FrontendBuildMode::PageComponents => println!("[Working on Frontend Page Components]"),
+            FrontendBuildMode::Completion => println!("[Working on Frontend Completion Items]"),
+        }
+    }
+}
